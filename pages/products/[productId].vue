690d2c9cb37c24fc97735d524d818f4ab81db9f5
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2024-02-29 10:46:14 +0100

    components

--- pages/products/[productId].vue
+++ pages/products/[productId].vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-<h2>Produkter {{ productId }}</h2>
+    <h2>Produkter {{ productId }}</h2>
+    <ProductDetails :product="productData" />
   </div>
 </template>
 
-<script setup >
+<script>
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
-const {productId} = useRoute().params
+export default defineComponent({
+  async setup() {
+    const firestore = inject("firestore");
+    const { productId } = useRoute().params;
 
+    const db = getFirestore(firestore);
+    const docRef = doc(db, "Product", productId);
+    const docSnap = await getDoc(docRef);
+    let productData = null;
+
+    if (docSnap.exists()) {
+      productData = docSnap.data();
+
+      console.log("Document data:", productData);
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
+
+    return { productId, productData };
+  },
+
+  //const {data: product} =
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
